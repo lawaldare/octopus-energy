d5763512c4932a4cbf5373e296d47388497fa8a5
lawal <lawal@ebi.ac.uk>
2026-01-24 15:22:58 +0000

    updated

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,11 @@
-import { Component, inject, signal } from '@angular/core';
+import { Component, computed, inject, signal } from '@angular/core';
 import { EnergyService } from './energy';
 import { toSignal } from '@angular/core/rxjs-interop';
 import { map } from 'rxjs';
 import { CommonModule, DatePipe } from '@angular/common';
 import { ConsumptionPricePipe } from './energy-pipe';
 import { DownloadFileTypeService } from './download-file-type.service';
+import { NEW_STANDING_CHARGE, NEW_UNIT_RATE } from './energy.constant';
 
 @Component({
   selector: 'app-root',
@@ -14,13 +15,30 @@ import { DownloadFileTypeService } from './download-file-type.service';
   providers: [DatePipe, ConsumptionPricePipe],
 })
 export class App {
+  private readonly NEW_DATE_START = new Date('2026-01-23T00:00:00Z');
   private readonly energyService = inject(EnergyService);
   private readonly consumpionPricePipe = inject(ConsumptionPricePipe);
   private readonly datePipe = inject(DatePipe);
   private readonly downloadFileTypeService = inject(DownloadFileTypeService);
   public readonly energyBillsByDay = toSignal(
-    this.energyService.getEnergyBillsByDay().pipe(map((data: any) => data.results))
+    this.energyService.getEnergyBillsByDay().pipe(
+      map((data: any) => {
+        return data.results.filter(
+          (bill: any) => new Date(bill.interval_start).getTime() >= this.NEW_DATE_START.getTime(),
+        );
+      }),
+    ),
   );
+
+  public total = computed(() => {
+    const bills = this.energyBillsByDay();
+    if (!bills) return;
+    const sum = bills.reduce((acc: number, bill: any) => {
+      const price = NEW_UNIT_RATE * bill.consumption + NEW_STANDING_CHARGE;
+      return acc + price;
+    }, 0);
+    return sum.toFixed(2);
+  });
 
   public exportToCSV(): void {
     const dataToExport = this.energyBillsByDay().map((bill: any, index: number) => {
@@ -30,7 +48,7 @@ export class App {
         'Consumption Unit (kWh)': bill.consumption,
         'Consumption Price (SC Inc.)': this.consumpionPricePipe.transform(
           bill.consumption,
-          bill.interval_start
+          bill.interval_start,
         ),
       };
     });
--- src/app/energy-pipe.ts
+++ src/app/energy-pipe.ts
@@ -1,17 +1,16 @@
 import { Pipe, PipeTransform } from '@angular/core';
+import {
+  NEW_UNIT_RATE,
+  OLD_UNIT_RATE,
+  NEW_STANDING_CHARGE,
+  OLD_STANDING_CHARGE,
+} from './energy.constant';
 
 @Pipe({
   name: 'consumpionPrice',
 })
 export class ConsumptionPricePipe implements PipeTransform {
   private readonly NEW_TARIFF_START = new Date('2026-01-03T00:00:00Z');
-
-  private readonly OLD_UNIT_RATE = 0.2877;
-  private readonly OLD_STANDING_CHARGE = 0.5455;
-
-  private readonly NEW_UNIT_RATE = 0.2594;
-  private readonly NEW_STANDING_CHARGE = 0.4729;
-
   // transform(value: string): number {
   //   const energyPrice = 0.2788;
   //   const consumptionInKWh = +parseFloat(value).toFixed(2);
@@ -26,8 +25,8 @@ export class ConsumptionPricePipe implements PipeTransform {
     const date = intervalStart ? new Date(intervalStart) : null;
     const isNewTariff = date ? date.getTime() >= this.NEW_TARIFF_START.getTime() : false;
 
-    const unitRate = isNewTariff ? this.NEW_UNIT_RATE : this.OLD_UNIT_RATE;
-    const standing = isNewTariff ? this.NEW_STANDING_CHARGE : this.OLD_STANDING_CHARGE;
+    const unitRate = isNewTariff ? NEW_UNIT_RATE : OLD_UNIT_RATE;
+    const standing = isNewTariff ? NEW_STANDING_CHARGE : OLD_STANDING_CHARGE;
 
     const total = unitRate * kwh + standing;
     return `£${Number(total.toFixed(2))}`;
